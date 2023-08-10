cd930e4e9f48eee023496418e3ec05e2861c660e
Joseph-Jnr <josephnwobodo1@gmail.com>
2023-08-10 19:40:15 +0100

    replaced ststic profile image with user.image

--- components/Profile.jsx
+++ components/Profile.jsx
@@ -1,14 +1,20 @@
-import { NextIcon } from '@public/assets'
+import { useSession } from 'next-auth/react'
 import Image from 'next/image'
-import React from 'react'
 import PromptCard from './PromptCard'
 
 const Profile = ({ name, desc, data, handleEdit, handleDelete }) => {
+  const { data: session } = useSession()
+
   return (
     <section className='w-full'>
       <div className='profile_wrap flex-center flex-col text-center gap-4'>
         <div className='profile_image'>
-          <Image src={NextIcon} width={100} />
+          <Image
+            src={session?.user.image}
+            className='rounded-full'
+            width={100}
+            height={100}
+          />
         </div>
         <div className='profile_details'>
           <h1 className='font-bold text-xl orange_gradient'>{name}</h1>
