cebd9f7972ce43c3f7883a24a000bb9d02db5b8d
Joseph-Jnr <josephnwobodo1@gmail.com>
2023-08-10 10:43:31 +0100

    profile page logic

--- app/profile/page.jsx
+++ app/profile/page.jsx
@@ -1,54 +1,39 @@
+'use client'
+
+import { useState, useEffect } from 'react'
+import { useSession } from 'next-auth/react'
+import { useRouter } from 'next/navigation'
 import { NextIcon } from '@public/assets'
 import Image from 'next/image'
+import { Profile } from '@components'
 
-const Profile = () => {
+const Profiles = () => {
+  const { data: session } = useSession()
+  const [posts, setPosts] = useState([])
+
+  useEffect(() => {
+    const fetchPosts = async () => {
+      const response = await fetch(`/api/users/${session?.user.id}/posts`)
+      const data = await response.json()
+
+      setPosts(data)
+    }
+
+    if (session?.user.id) fetchPosts()
+  }, [])
+
+  const handleEdit = () => {}
+  const handleDelete = async () => {}
+
   return (
-    <section>
-      <div className='profile_wrap flex flex-col flex-center text-center'>
-        <div className='profile_image'>
-          <Image src={NextIcon} width={100} />
-        </div>
-        <div className='profile_details mt-5'>
-          <h1 className='font-bold text-xl orange_gradient'>Joseph Jnr</h1>
-          <p className='text-slate-500 text-sm'>josephnwobodo1@gmail.com</p>
-        </div>
-
-        <div className='proile_btns mt-3'>
-          <button className='black_btn'>Edit Profile</button>
-        </div>
-      </div>
-
-      <div className='my-10 p-3 bg-gray-200 border border-gray-300 rounded-lg w-96'>
-        <div className='single_profile_detail my-5'>
-          <h2 className='font-bold'>Username</h2>
-          <p className='text-sm border border-gray-400 p-2 rounded-md text-slate-600 mt-2'>
-            Joseph Jnr
-          </p>
-        </div>
-
-        <div className='single_profile_detail my-5'>
-          <h2 className='font-bold'>Email</h2>
-          <p className='text-sm border border-gray-400 p-2 rounded-md text-slate-600 mt-2'>
-            josephnwobodo1@gmail.com
-          </p>
-        </div>
-
-        <div className='single_profile_detail my-5'>
-          <h2 className='font-bold'>Github</h2>
-          <p className='text-sm border border-gray-400 p-2 rounded-md text-slate-600 mt-2'>
-            @Joseph-Jnr
-          </p>
-        </div>
-
-        <div className='single_profile_detail my-5'>
-          <h2 className='font-bold'>Phone Number</h2>
-          <p className='text-sm border border-gray-400 p-2 rounded-md text-slate-600 mt-2'>
-            +22999830482
-          </p>
-        </div>
-      </div>
-    </section>
+    <Profile
+      name='My'
+      desc='Welcome to your personalized profile page'
+      data={posts}
+      handleEdit={handleEdit}
+      handleDelete={handleDelete}
+    />
   )
 }
 
-export default Profile
+export default Profiles
--- components/Profile.jsx
+++ components/Profile.jsx
@@ -1,7 +1,53 @@
 import React from 'react'
 
 const Profile = () => {
-  return <div></div>
+  return (
+    <section>
+      <div className='profile_wrap flex flex-col flex-center text-center'>
+        <div className='profile_image'>
+          <Image src={NextIcon} width={100} />
+        </div>
+        <div className='profile_details mt-5'>
+          <h1 className='font-bold text-xl orange_gradient'>Joseph Jnr</h1>
+          <p className='text-slate-500 text-sm'>josephnwobodo1@gmail.com</p>
+        </div>
+
+        <div className='proile_btns mt-3'>
+          <button className='black_btn'>Edit Profile</button>
+        </div>
+      </div>
+
+      <div className='my-10 p-3 bg-gray-200 border border-gray-300 rounded-lg w-96'>
+        <div className='single_profile_detail my-5'>
+          <h2 className='font-bold'>Username</h2>
+          <p className='text-sm border border-gray-400 p-2 rounded-md text-slate-600 mt-2'>
+            Joseph Jnr
+          </p>
+        </div>
+
+        <div className='single_profile_detail my-5'>
+          <h2 className='font-bold'>Email</h2>
+          <p className='text-sm border border-gray-400 p-2 rounded-md text-slate-600 mt-2'>
+            josephnwobodo1@gmail.com
+          </p>
+        </div>
+
+        <div className='single_profile_detail my-5'>
+          <h2 className='font-bold'>Github</h2>
+          <p className='text-sm border border-gray-400 p-2 rounded-md text-slate-600 mt-2'>
+            @Joseph-Jnr
+          </p>
+        </div>
+
+        <div className='single_profile_detail my-5'>
+          <h2 className='font-bold'>Phone Number</h2>
+          <p className='text-sm border border-gray-400 p-2 rounded-md text-slate-600 mt-2'>
+            +22999830482
+          </p>
+        </div>
+      </div>
+    </section>
+  )
 }
 
 export default Profile
