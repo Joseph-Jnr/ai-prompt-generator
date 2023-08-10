8f5e312d88939fde2e5a63a4be64d191127b253d
Joseph-Jnr <josephnwobodo1@gmail.com>
2023-08-10 13:03:44 +0100

    added update and delete buttons to PromptCard

--- components/PromptCard.jsx
+++ components/PromptCard.jsx
@@ -5,8 +5,12 @@ import { useSession } from 'next-auth/react'
 import { usePathname, useRouter } from 'next/navigation'
 import { CopyIcon, TickIcon } from '@public/assets'
 import Image from 'next/image'
+import { IconEdit, IconTrash } from '@tabler/icons-react'
 
 const PromptCard = ({ post, handleTagClick, handleEdit, handleDelete }) => {
+  const { data: session } = useSession()
+  const pathName = usePathname()
+  const router = useRouter()
   const [copied, setCopied] = useState('')
 
   const handleCopy = () => {
@@ -19,18 +23,37 @@ const PromptCard = ({ post, handleTagClick, handleEdit, handleDelete }) => {
     <div className='my-20'>
       <div className='glassmorphism'>
         <div className='flex gap-3 flex-start'>
-          <Image src={post.creator.image} className='rounded-full' width={30} />
+          <Image
+            src={post.creator.image}
+            className='rounded-full'
+            width={30}
+            height={30}
+          />
           <div className='details'>
             <div className='header flex flex-between items-center'>
               <div className='user-info'>
-                <h1 className='font-bold'>{post.creator.username}</h1>
+                <h1 className='font-bold capitalize'>
+                  {post.creator.username}
+                </h1>
                 <p className='text-sm text-slate-500'>{post.creator.email}</p>
               </div>
-              <div className='icon' onClick={handleCopy}>
-                <Image
-                  src={copied === post.prompt ? TickIcon : CopyIcon}
-                  className='cursor-pointer'
-                />
+              <div className='flex-center gap-4'>
+                {session?.user.id === post.creator._id &&
+                  pathName === '/profile' && (
+                    <div
+                      className='edit_post cursor-pointer'
+                      onClick={handleEdit}
+                    >
+                      <IconEdit color='green' stroke={1} size={20} />
+                    </div>
+                  )}
+
+                <div className='icon' onClick={handleCopy}>
+                  <Image
+                    src={copied === post.prompt ? TickIcon : CopyIcon}
+                    className='cursor-pointer'
+                  />
+                </div>
               </div>
             </div>
             <div className='description text-sm mt-5 text-slate-700'>
@@ -43,10 +66,22 @@ const PromptCard = ({ post, handleTagClick, handleEdit, handleDelete }) => {
                   className='rounded-full bg-gray-200 border border-gray-300 text-black p-2 text-xs'
                   onClick={() => handleTagClick && handleTagClick(post.tag)}
                 >
-                  {post.tag}
+                  <span>#{post.tag}</span>
                 </div>
               </div>
             </div>
+
+            {session?.user.id === post.creator._id &&
+              pathName === '/profile' && (
+                <div className='flex flex-end mt-3'>
+                  <div
+                    className='delete_post cursor-pointer bg-red-100 hover:bg-red-200 p-2 rounded'
+                    onClick={handleDelete}
+                  >
+                    <IconTrash color='red' stroke={1} size={20} />
+                  </div>
+                </div>
+              )}
           </div>
         </div>
       </div>
