bb2e8fffc9362064c1d99c88b360b31827e502e2
Joseph-Jnr <josephnwobodo1@gmail.com>
2023-08-11 12:53:21 +0100

    reduced scrollbar width

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -6,7 +6,7 @@ const Hero = () => {
         <span className='orange_gradient text-center'>AI-Powered Prompts</span>
       </h1>
       <p className='desc text-center'>
-        Promptopia is an open-source AI prompting tool for modern world to
+        PromptAI is an open-source AI prompting tool for modern world to
         discover, create and share creative prompts.
       </p>
     </>
